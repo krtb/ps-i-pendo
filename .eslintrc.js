879b82618f211edeb31af01cf6bfb7caadecc437
Kurt <kurt.bauer@pendo.io>
2021-12-06 23:33:26 -0500

    add import extensions, ignorePAtrerns to ignore import without .js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
+  settings: {
+    'import/extensions': ['.js', '.mjs', '.jsx', '.ts', '.tsx'],
+  },
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: [
@@ -8,10 +11,18 @@ module.exports = {
     'airbnb-base',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  ignorePatterns: [
+    'node_modules/',
   ],
   rules: {
     semi: ['error', 'always'],
     quotes: ['error', 'single'],
+    indent: ['error', 2],
+    'import/extensions': 'off',
     'import/no-extraneous-dependencies': 0,
     'no-console': 'off',
   },
