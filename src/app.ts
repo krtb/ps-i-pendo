89747df71b9f1bebd9b1a65be04ec11655826fea
Kurt <kurt.bauer@pendo.io>
2021-12-13 18:18:46 -0500

    add middleware functions to get and post segments for migrations

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,18 @@
 import express from 'express';
 import 'dotenv/config';
-import { getASingleSegmentID } from '../middleware/segmentMiddleware';
+import { postNewSegments, getSegmentsFromUser } from '../middleware/segmentMiddleware';
 
 const app: express.Application = express();
 
 const port = process.env.SERVER_PORT_3000;
 
-app.use(getASingleSegmentID);
+app.use(getSegmentsFromUser);
+app.use(postNewSegments);
 
 app.get('/', (_req, _res) => {
   _res.send('TypeScript Wiht Expresss');
 });
 
 app.listen(port, () => {
-  console.log(`TypeScript with Expresshttp://localhost:${port}/`);
+  console.log(`Server available at http://localhost:${port}/`);
 });
